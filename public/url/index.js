8ed428b438b196bf3bb2d6bd93dbbf68cf52ce74
Sebastian Guillier Asus <sebastianguillier@gmail.com>
2022-03-27 03:08:37 -0300

    [commit - 03] - cambio de URL

--- public/url/index.js
+++ public/url/index.js
@@ -1,4 +1,4 @@
-const URL_RAIZ =  "https://back-end-proyecto-final.herokuapp.com/"
+const URL_RAIZ =  "https://back-end-proyecto-final.herokuapp.com"
 //const URL_RAIZ =  "http://localhost:8080"
 
 export { URL_RAIZ }
